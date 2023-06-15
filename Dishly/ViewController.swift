2e839096e80d188e8f9255345df487319226c43e
Vladimir Kovalev <vovakovalev2016@icloud.com>
2023-06-15 16:47:58 +0300

    initial commit

---
+++ Dishly/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Dishly
+//
+//  Created by Vladimir Kovalev on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
